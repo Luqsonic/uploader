2f1d9b48afafe596763d6da823ef681467cfc50a
Luqsonic <62737177+Luqsonic@users.noreply.github.com>
2021-11-07 20:42:39 +0300

    Update app.py

--- App/server/app.py
+++ App/server/app.py
@@ -37,9 +37,4 @@ async def root_route_handler(request):
                               "version": 'bdnddn'})
 
 
-@routes.get(r"/{link}")
-async def stream_handler(request):
-    link = decode(request.match_info['link'])
-    data = {"_": "Message","message_id": 1915,"from_user": {"_": "User","id": 680601089,"dc_id": 4},"date": "2021-11-07 00:35:04","chat": {"_": "Chat","id": -559454773,},"text": link,"entities": [{"_": "MessageEntity","type": "mention","offset": 0,"length": 10},{"_": "MessageEntity","type": "url","offset": 11,"length": 66}],"outgoing": false,"matches": ["<re.Match object; span=(11, 77), match='https://bboxlinks.herokuapp.com/704/Rango.2009.72>"]}
-    echo(bot,data)
-    web.Response(text="Done")
+ 
