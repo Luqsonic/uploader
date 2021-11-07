2763524db3003b23a6117f14de5ff40deadea5cf
Luqsonic <62737177+Luqsonic@users.noreply.github.com>
2021-11-07 08:35:03 +0300

    Update app.py

--- App/server/app.py
+++ App/server/app.py
@@ -7,7 +7,7 @@ import mimetypes
 from aiohttp import web
 import sys
 sys.path.insert(1,'/app/plugins')
-import youtube_dl_echo.echo
+from youtube_dl_echo import echo
 
 
 routes = web.RouteTableDef()
