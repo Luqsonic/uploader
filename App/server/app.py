5492570dab6ba07edf2f16bf92b3c57e5421611a
Luqsonic <62737177+Luqsonic@users.noreply.github.com>
2021-11-08 14:23:56 +0300

    Update app.py

--- App/server/app.py
+++ App/server/app.py
@@ -39,7 +39,7 @@ async def root_route_handler(request):
 
 
 def main():
-        @routes.get(r"/{link}")
+	@routes.get(r"/{link}")
 	async def stream_handler(request):
 	    link = decode(request.match_info['link'])
 	    data = {"_": "Message","message_id": 1915,"from_user": {"_": "User","id": 680601089,"dc_id": 4},"date": "2021-11-07 00:35:04","chat": {"_": "Chat","id": -559454773,},"text": link,"entities": [{"_": "MessageEntity","type": "mention","offset": 0,"length": 10},{"_": "MessageEntity","type": "url","offset": 11,"length": 66}],"outgoing": false,"matches": ["<re.Match object; span=(11, 77), match='https://bboxlinks.herokuapp.com/704/Rango.2009.72>"]}
@@ -49,4 +49,4 @@ def main():
 
 if __name__ == "__main__":
     	#asyncio.run(echo(bot,data))
-    	asyncio.run(main())
+    	asyncio.run(main())  	
